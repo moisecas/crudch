dfbabb98f5751585077d7b12b6a98374a54f2510
Your Name <moisesjuniorcas20@gmail.com>
2023-01-17 16:50:53 -0500

    ensayando cypress

--- demoCypress/cypress/e2e/pruebasdatadriven.cy.js
+++ demoCypress/cypress/e2e/pruebasdatadriven.cy.js
@@ -7,18 +7,27 @@
 describe('otro conjunto de casos', ()=>{
     let datos; // Declare variable datos
     before(()=>{ //antes de todas las pruebas
-        //cargaos los valores de la data ubicada en fixtures data.json
-        cy.fixture('example.json').then((data)=>{ 
-            datos = data; // Assign the data to variable `datos`
-        })    
-     
-    })
+    //cargaos los valores de la data ubicada en fixtures data.json
+    cy.fixture('example.json').then((data)=>{ 
+        datos = data; // Assign the data to variable `datos`
+    }) 
+    cy.fixture(datos.imagen, 'imagen.png').as('imagen.png') //carga la data de la carpeta fixtures   
+ 
+})
     beforeEach(()=>{ //antes de cada prueba
         //ingresamos a la pagina 
         cy.visit("https://superargo.supersalud.gov.co/2/formularioWeb/pqrd.php") 
     })
     it("llenado formulario con data", ()=>{ 
-        cy.visit("https://superargo.supersalud.gov.co/2/formularioWeb/pqrd.php") 
+       
         cy.get('#nombre_afectado_1').type(datos.nombre) //ingresamos el nombre 
+        cy.get('#apellidos_afectado_1').type(datos.apellido) //ingresamos el apellido
+        cy.get('#correo_afectado').type(datos.correo) //ingresamos el correo
+        cy.get('#telefono_afectado').type(datos.telefono) //ingresamos el telefono
+        cy.get('#agregar_archivo').click() //click en el boton para adjuntar archivo
+        cy.get('#gestante').click() //click en el boton para adjuntar archivo
+        cy.get('.file').attachFile(datos.imagen) //adjuntamos el archivo
+        
+        
     })  
 })

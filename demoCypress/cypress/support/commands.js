ad7aa9107330f0e07642e4d6bdb034c2ef783969
Your Name <moisesjuniorcas20@gmail.com>
2023-01-19 16:16:24 -0500

    ensayando cypress

--- demoCypress/cypress/support/commands.js
+++ demoCypress/cypress/support/commands.js
@@ -23,3 +23,18 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("formulario", () => {
+    for (let i = 0; i < 5; i++) {
+        cy.visit("https://docs.google.com/forms/d/e/1FAIpQLSf0_6SB7STsPC_kGeEPTWfx9MA6uRFYSSxKOBJpsqZN1arQKA/viewform")
+        cy.get('#i17 > .vd3tt > .AB7Lab').click()
+        cy.get('#i48 > .vd3tt > .AB7Lab').click()
+        cy.get('.lRwqcd > .uArJ5e > .l4V7wb > .NPEfkd').click()
+        cy.get('#i67 > .vd3tt > .AB7Lab').click() 
+        cy.get('#i77 > .vd3tt > .AB7Lab').click()
+        cy.get('#i93 > .vd3tt > .AB7Lab').click()
+        cy.get('#i112 > .vd3tt > .AB7Lab').click()
+        cy.get('.lRwqcd > .uArJ5e > .l4V7wb > .NPEfkd').click()
+
+    }
+}) 

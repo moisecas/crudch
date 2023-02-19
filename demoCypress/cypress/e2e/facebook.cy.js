50c98e5e7dab4770dfad253db99fcea053e14076
Your Name <moisesjuniorcas20@gmail.com>
2023-02-18 22:44:32 -0500

    intento por automatizar compartir

---
+++ demoCypress/cypress/e2e/facebook.cy.js
@@ -0,0 +1,7 @@
+describe("conjunto de caso de prueba 1", function(){
+    it("validar pagina de inicio", function(){
+        cy.visit("https://www.facebook.com/photo/?fbid=195576293151426&set=a.189308857111503")
+        
+    })
+
+})
